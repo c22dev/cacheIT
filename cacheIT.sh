2056abe9cab29ad9428292507d9848557ec7b4b0
Constantin Clerc <102235607+c22dev@users.noreply.github.com>
2023-11-02 00:08:13 +0100

    Update cacheIT.sh

--- cacheIT.sh
+++ cacheIT.sh
@@ -1,36 +1,60 @@
 #!/bin/bash
 
-# cacheIT! - v0.1
+# cacheIT! - v0.2
 # Constantin CLERC
 
-# This script make a cache copy of any modification in the Printers directory
+# This script makes a cache copy of any modification in the Printers directory
 # Feel free to edit the monitored folder, as well as the cache folder (you can hide it more)
+
+# 0.2 brings a rewrite as well as an app update selection system. Please update your sh script now by deleting current .cacheIT! hidden directory in your home folder.
+# What's new ?
+# Check for EVERY FILES AND FOLDERS, not only .app. This make file structure a lot more accurate and files are correctly copied.
+# Update system - allow user to update apps (if app in cache is older than app in monitored)
 
 # Path to the monitored folder
 MONITORED_FOLDER="/Users/etudiant/Library/Printers/"
 
 # Path to the cache folder
 CACHE_FOLDER="/Users/etudiant/.cacheIT!/"
+
 # Infinite loop to continuously monitor the folder
 while true; do
-    # Get a list of .app folders in the monitored folder
-    current_apps=$(find "$MONITORED_FOLDER" -name "*.app" -type d)
+    # Get a list of all files and folders in the monitored folder, including hidden ones
+    current_items=$(find "$MONITORED_FOLDER" -mindepth 1)
 
-    # Check for added .app folders
-    for app_folder in $current_apps; do
-        # Check if the app folder is not in the cache folder
-        if [ ! -d "$CACHE_FOLDER/$(basename "$app_folder")" ]; then
-            # Copy the app folder to the cache folder
-            cp -r "$app_folder" "$CACHE_FOLDER"
+    # Check for added or modified items
+    for item in $current_items; do
+        # Extract the relative path from the monitored folder
+        relative_path=$(realpath --relative-to="$MONITORED_FOLDER" "$item")
+
+        # Check if the item is not in the cache folder
+        if [ ! -e "$CACHE_FOLDER/$relative_path" ]; then
+            # Copy the item to the cache folder
+            cp -R "$item" "$CACHE_FOLDER/$relative_path"
+        elif [ -d "$item" ]; then
+            # Check if the item in the cache folder is older than the monitored item
+            if [ "$item" -nt "$CACHE_FOLDER/$relative_path" ]; then
+                # Update the directory in the cache folder
+                cp -R "$item" "$CACHE_FOLDER/$relative_path"
+            fi
+        else
+            # Check if the item in the cache folder is older than the monitored item
+            if [ "$item" -nt "$CACHE_FOLDER/$relative_path" ]; then
+                # Update the file in the cache folder
+                cp -R "$item" "$CACHE_FOLDER/$relative_path"
+            fi
         fi
     done
 
-    # Check for deleted .app folders
-    for cached_app_folder in "$CACHE_FOLDER"/*.app; do
-        # Check if the cached app folder is not in the monitored folder
-        if [ ! -d "$MONITORED_FOLDER/$(basename "$cached_app_folder")" ]; then
-            # Copy the app folder back to the monitored folder
-            cp -r "$cached_app_folder" "$MONITORED_FOLDER"
+    # Check for deleted items
+    for cached_item in "$CACHE_FOLDER"/*; do
+        # Extract the relative path from the cache folder
+        relative_path=$(realpath --relative-to="$CACHE_FOLDER" "$cached_item")
+
+        # Check if the cached item is not in the monitored folder
+        if [ ! -e "$MONITORED_FOLDER/$relative_path" ]; then
+            # Restore the item from the cache folder
+            cp -R "$cached_item" "$MONITORED_FOLDER/$relative_path"
         fi
     done
 
